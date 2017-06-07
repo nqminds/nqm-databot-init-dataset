e376902a7aac314dd00b669002c9dfbc1de4e304
Alexandru Mereacre <mereacre@gmail.com>
2017-06-07 16:03:49 +0100

    Added functions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 (function() {
   "use strict";
 
-  const databot = require("./lib/init-datasets");
+  const databot = require("./lib/wahsn-functions");
   const input = require("nqm-databot-utils").input;
   input.pipe(databot);
 }());
